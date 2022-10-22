f3e1d604fb814499cb7b868ccf13c9fde901d294
baokhanh2707 <baokhanhjax@gmail.com>
2022-10-22 16:05:23 +0700

    nop_bai_tap

--- module3/case_study/information_furama_resort.sql
+++ module3/case_study/information_furama_resort.sql
@@ -22,7 +22,7 @@ value
 insert into nhan_vien(ma_nhan_vien, ho_ten, ngay_sinh, so_cmnd, luong, so_dien_thoai, email, dia_chi, ma_vi_tri, ma_trinh_do, ma_bo_phan)
 value
 (1,"Nguyễn Văn An","1970-11-07","4562317861",10000000,"0901234121","annguyen@gamil.com","295 Nguyễn Tất Thành, Đà Nẵng",1,3,1),
-(2,"Lê Văn Bình","1997-04-09","654231234",70000000,"0934212314","binhlv@gmail.com","22 Yên Bái, Đà Nẵng",1,2,2),
+(2,"Lê Văn Bình","1997-04-09","654231234",70000000,"0934212314","binhlv@gmail.com","22 Yên Bái, Đà Nẵng",1,2,2),
 (3,"Hồ Thị Yến","1995-12-12","999231723",14000000,"0412352315","thiyen@gamil.com","K234/11 Điện Biên Phủ, Gia Lai",1,3,2),
 (4,"Võ Công Toản","1980-04-04","123231365",17000000,"0374443232","toan0404@gmail.com","77 Hoàng Diệu, Quảng Trị",1,4,4),
 (5,"Nguyễn Bỉnh Phát","1999-12-09","454363232",6000000,"0902341231","phatphat@gmail.com","43 Yên Bái, Đà Nẵng",2,1,1),
--- module3/case_study/query_furama_15_28.sql
+++ module3/case_study/query_furama_15_28.sql
@@ -55,7 +55,7 @@ WHERE
         );
         
      --    task 18
-     set sql_safe_updates = 0;
+     
      SET FOREIGN_KEY_CHECKS=0;
      delete  from khach_hang
      where  ma_khach_hang  IN (SELECT 
@@ -66,16 +66,24 @@ WHERE
         YEAR(hop_dong.ngay_lam_hop_dong) < '2021' );
         
        --  task 19
-       
-	update dich_vu_di_kem 
-    set gia = gia *2
-    where (select*from (select  sum(hop_dong_chi_tiet.so_luong) as so_lan_su_dung from hop_dong_chi_tiet 
-    join dich_vu_di_kem on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
-    join hop_dong on hop_dong_chi_tiet.ma_hop_dong=hop_dong.ma_hop_dong
-    where year(hop_dong.ngay_lam_hop_dong) = '2020' 
-    group by dich_vu_di_kem.ma_dich_vu_di_kem
-    having so_lan_su_dung > 10 ) as x
-    );
+       set sql_safe_updates = 0;
+	UPDATE dich_vu_di_kem 
+SET 
+    gia = gia * 2
+WHERE
+    (SELECT 
+            *
+        FROM
+            (SELECT 
+                SUM(hop_dong_chi_tiet.so_luong) AS so_lan_su_dung
+            FROM
+                hop_dong_chi_tiet
+            JOIN dich_vu_di_kem ON dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+            JOIN hop_dong ON hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+            WHERE
+                YEAR(hop_dong.ngay_lam_hop_dong) = '2020'
+            GROUP BY dich_vu_di_kem.ma_dich_vu_di_kem
+            HAVING so_lan_su_dung > 10) AS x);
     
 
      -- task 20
@@ -97,7 +105,45 @@ UNION SELECT
     ngay_sinh,
     dia_chi
 FROM
-    khach_hang
-    
-          
+    khach_hang ;
+     
+  --   task 21
+  
+ create view v_nhan_vien as
+ select nhan_vien.*   from nhan_vien
+join hop_dong on nhan_vien.ma_nhan_vien =  hop_dong.ma_nhan_vien
+ where (hop_dong.ngay_lam_hop_dong) between '2021-04-01' and '2021-04-30' and nhan_vien.dia_chi like '% Đà Nẵng' ;
 
+ SELECT * FROM v_nhan_vien;
+drop view v_nhan_vien;
+
+-- task 22
+set sql_safe_updates = 0;
+UPDATE nhan_vien 
+SET 
+    dia_chi = 'Liên Chiểu , Đà Nẵng'
+WHERE ma_nhan_vien in ( select * from( select v_nhan_vien.ma_nhan_vien from v_nhan_vien) as a);
+    
+-- task 23 
+
+Delimiter //
+create procedure sp_xoa_khach_hang(ma_khach_hang  int)
+    begin
+    delete from khach_hang
+    where khach_hang.ma_khach_hang = ma_khach_hang;
+   end //
+DELIMITER ;
+
+call sp_xoa_khach_hang (3)
+
+-- task 24
+
+Delimiter //
+create procedure sp_them_moi_hop_dong ()
+    begin
+    insert into hop_dong 
+    value (13,'2022-04-01','2022-04-30',0,10,10,6);
+   end //
+DELIMITER ;
+
+call sp_them_moi_hop_dong ()
