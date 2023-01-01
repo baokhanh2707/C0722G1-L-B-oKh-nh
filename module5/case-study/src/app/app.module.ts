1f2db434db04809b837db6839892012b096a05fc
baokhanh2707 <baokhanhjax@gmail.com>
2023-01-01 12:30:18 +0700

    ok

--- module5/case-study/src/app/app.module.ts
+++ module5/case-study/src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { NavigationComponent } from './layout/navigation/navigation.component';
-import { FacilityListComponent } from './facility/list/facility-list.component';
-import { FacilityEditComponent } from './facility/edit/facility-edit.component';
-import { FacilityCreateComponent } from './facility/create/facility-create.component';
-import { ListComponent } from './customer/list/list.component';
-import { CreateComponent } from './customer/create/create.component';
-import { EditComponent } from './customer/edit/edit.component';
-import { ContractCreateComponent } from './contract/create/contract-create.component';
-import { BodyComponent } from './layout/body/body.component';
-import { ContractListComponent } from './contract/list/contract-list.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {FooterComponent} from './layout/footer/footer.component';
+import {NavigationComponent} from './layout/navigation/navigation.component';
+import {FacilityListComponent} from './facility/list/facility-list.component';
+import {FacilityEditComponent} from './facility/edit/facility-edit.component';
+import {FacilityCreateComponent} from './facility/create/facility-create.component';
+import {ListComponent} from './customer/list/list.component';
+import {CreateComponent} from './customer/create/create.component';
+import {EditComponent} from './customer/edit/edit.component';
+import {ContractCreateComponent} from './contract/create/contract-create.component';
+import {BodyComponent} from './layout/body/body.component';
+import {ContractListComponent} from './contract/list/contract-list.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -33,12 +34,14 @@ import {ReactiveFormsModule} from "@angular/forms";
     BodyComponent,
     ContractListComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
